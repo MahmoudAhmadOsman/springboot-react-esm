082787dc02813d63cd0d7c5c6ccb6e236fe9e082
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-01-06 00:12:29 -0600

    added EmployeeList component

--- frontend/src/components/EmployeeListComponent.js
+++ frontend/src/components/EmployeeListComponent.js
@@ -4,26 +4,67 @@ import { Link } from "react-router-dom";
 import EmployeeService from "../service/EmployeeService";
 
 const EmployeeListComponent = () => {
+	const [employees, setEmployees] = useState([]);
+
+	//1.
+	useEffect(() => {
+		getAllEmployees();
+	}, []);
+
+	//2.
+
+	const getAllEmployees = () => {
+		EmployeeService.getAllEmployees()
+			.then((res) => {
+				setEmployees(res.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	function deleteEmployee(e, id) {
+		e.preventDefault();
+	}
+
 	return (
 		<section className="employee-list">
 			<div className="container mt-5">
+				<h2 className="text-success">List of Employees</h2> <hr />
 				<div className="row">
 					<table className="table">
 						<thead>
 							<tr>
 								<th scope="col">ID #</th>
-								<th scope="col">First</th>
-								<th scope="col">Last</th>
-								<th scope="col">Email</th>
+								<th scope="col">First Name</th>
+								<th scope="col">Last Name</th>
+								<th scope="col">Email Address</th>
+								<th>Actions</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<th scope="row">1</th>
-								<td>Mark</td>
-								<td>Otto</td>
-								<td>@mdo</td>
-							</tr>
+							{employees.map((employee) => (
+								<tr key={employee.id}>
+									<td>{employee.id}</td>
+									<td>{employee.firstName}</td>
+									<td>{employee.lastName}</td>
+									<td>{employee.email}</td>
+									<td>
+										<Link
+											to={`/add-employee/${employee.id}`}
+											className="btn btn-sm btn-success mr-2"
+										>
+											Update
+										</Link>
+										<a
+											onClick={(e) => deleteEmployee(e, employee.id)}
+											className="btn btn-sm btn-danger"
+										>
+											Delete
+										</a>
+									</td>
+								</tr>
+							))}
 						</tbody>
 					</table>
 				</div>
