2830e143cc74e02aca45883e69d44ec71d53384e
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-01-09 00:50:45 -0600

    added html form to AddEmployeeComponent component

--- frontend/src/components/AddEmployeeComponent.js
+++ frontend/src/components/AddEmployeeComponent.js
@@ -1,7 +1,53 @@
 import React from "react";
 
 const AddEmployeeComponent = () => {
-	return <div>AddEmployeeComponent</div>;
+	return (
+		<>
+			<div className="container mt-3">
+				<div className="row">
+					<div className="col-md-6 mx-auto">
+						<h2 className="text-success mb-3">Add New Employee</h2> <hr />{" "}
+						<br />
+						<form>
+							<div className="mb-3 mt-">
+								<label htmlFor="firstName">First Name</label>
+								<input
+									type="text"
+									className="form-control form-control-lg"
+									id="firstName"
+									placeholder="Enter your first name"
+									name="firstName"
+								/>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="lastName">Last Name</label>
+								<input
+									type="text"
+									className="form-control form-control-lg"
+									id="lastName"
+									placeholder="Enter your first name"
+									name="lastName"
+								/>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="email">Email Address</label>
+								<input
+									type="email"
+									className="form-control form-control-lg"
+									id="email"
+									placeholder="Enter your email"
+									name="email"
+								/>
+							</div>
+							<button type="submit" className="btn btn-success mb-3">
+								SUBMIT
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AddEmployeeComponent;
